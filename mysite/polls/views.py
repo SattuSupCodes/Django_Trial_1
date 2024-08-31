cec9c4c91f961a2b0565af12f0b0e45925b05945
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-31 17:13:08 +0530

    upd

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,10 +5,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-   return render(request, 'index.html')
-def hello(request):
-    return HttpResponse("hello")
+   context = { 'name' : 'Sattu',
+              'age': 20,
+               'nation':'India'}
+   return render(request, 'index.html', context)
 
+   
+def hello(request):
+   pass
 
 
 # i feel sooooooooooooooooooooooooooooooooooooooo fucked but hey, that's life of an engineer
